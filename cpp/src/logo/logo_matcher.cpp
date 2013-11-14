af497286bc64d73a09d4da34bf384356373e17f6
Tony Kovanen <tonykovanen@hotmail.com>
2013-11-14 11:31:10 +0200

    Fixed FlannBasedMatcher problem

--- cpp/src/logo/logo_matcher.cpp
+++ cpp/src/logo/logo_matcher.cpp
@@ -19,26 +19,25 @@ double matchLogosFreak(std::string sent_file_path, std::string reference_image_p
     freak.compute(sent, sent_keypoints, sent_descriptors);
     freak.compute(reference, reference_keypoints, reference_descriptors);
 
-    //FlannBasedMatcher matcher;
-    //std::vector<DMatch> matches;
-    //matcher.match(sent_descriptors, reference_descriptors, matches);
-    //
-    //double max_dist = 0, min_dist = 100;
-    //for (int i = 0; i < sent_descriptors.rows; i++) {
-    //    double dist = matches[i].distance;
-    //    max_dist = (dist > max_dist) ? dist : max_dist;
-    //    min_dist = (dist < min_dist) ? dist : min_dist;
-    //}
+    BFMatcher matcher(NORM_HAMMING);
+    std::vector<DMatch> matches;
+    matcher.match(sent_descriptors, reference_descriptors, matches);
+    
+    double max_dist = 0, min_dist = 100;
+    for (int i = 0; i < sent_descriptors.rows; i++) {
+        double dist = matches[i].distance;
+        max_dist = (dist > max_dist) ? dist : max_dist;
+        min_dist = (dist < min_dist) ? dist : min_dist;
+    }
 
-    //std::vector<DMatch> good_matches;
-    //for (int i = 0; i < sent_descriptors.rows; i++) {
-    //    if (matches[i].distance <= 2*min_dist) {
-    //        good_matches.push_back(matches[i]);
-    //    }
-    //}
+    std::vector<DMatch> good_matches;
+    for (int i = 0; i < sent_descriptors.rows; i++) {
+        if (matches[i].distance <= 2*min_dist) {
+            good_matches.push_back(matches[i]);
+        }
+    }
 
-    //return good_matches.size() * 1.0 / matches.size();
-    return 0.0;
+    return good_matches.size() * 1.0 / matches.size();
 }
 
 double matchLogosSurf(std::string sent_file_path, std::string reference_image_path) {
